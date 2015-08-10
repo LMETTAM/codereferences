d1da709a61e8a643b245ac930d9cc10cbdde3f20
lakshmi <mettam.lakshmi@gmail.com>
2015-08-10 19:28:14 -0400

    Changed the logic

--- src/com/text/coreexamples/First10numbers.java
+++ src/com/text/coreexamples/First10numbers.java
@@ -4,11 +4,16 @@ public class First10numbers {
 
 public static void main(String[] args){
 		
+	// initialize integer variable
 		int bag = 0;
 		
-		for(int i=1; i<=10;i++){
+		/*for(int i=1; i<=10;i++){
 			
 			bag = bag+i;
+		}*/
+		while(bag < 10){
+			System.out.println("current value: " + bag);
+			bag++;
 		}
 		
 		System.out.println(bag);
